f40cb4f7047f32291d562dfcac7087ab2d953323
satyam7513 <satyamknaidu@gmail.com>
2025-09-12 11:38:52 +0530

    conversation

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "hello-world!"
+echo "Trump:: Hey Musk, How are you?"
+echo "Musk:: Hello Trump, I am fine. How are you doing"
+echo "Trump: I am fine too. What's up?"
+echo "Musk:: Nohting, just going to Mars now, will you come?"
+echo "Trump:: Sorry, you carry on! I will come once you come back"
