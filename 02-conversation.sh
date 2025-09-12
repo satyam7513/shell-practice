a70f880d8d2fadc65858d3403bf6e60ffdeac7a0
satyam7513 <satyamknaidu@gmail.com>
2025-09-12 11:45:56 +0530

    variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Trump:: Hey Musk, How are you?"
-echo "Musk:: Hello Trump, I am fine. How are you doing"
-echo "Trump: I am fine too. What's up?"
-echo "Musk:: Nohting, just going to Mars now, will you come?"
-echo "Trump:: Sorry, you carry on! I will come once you come back"#!/bin/bash
+PERSON1=Modi #there should be no space before and after =
+PERSON2=Musk
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
+echo "$PERSON1:: I am fine too. What's up?"
+echo "$PERSON2:: Nohting, just going to Mars now, will you come?"
+echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
