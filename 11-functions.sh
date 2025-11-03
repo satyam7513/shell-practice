319d5d15c61ed2990eb7b1db92e9e74fc0533195
satyam7513 <satyamknaidu@gmail.com>
2025-11-03 18:38:24 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,56 +5,48 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
-    exit 1 
+    exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
 fi
 
-#VALIDATE Functions take input as exit status
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-    if [ $1 -eq 0]
+    if [ $1 -eq 0 ]
     then
-       echo "Installing $2 is .. SUCCESS"
+        echo "Installing $2 is ... SUCCESS"
     else
-       echo "Installing $2 is ...FAILURE"
-       exit 1
+        echo "Installing $2 is ... FAILURE"
+        exit 1
     fi
 }
-
-
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
     echo "MySQL is not installed... going to install it"
     dnf install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "Installing MySQL is ... SUCCESS"
-    else
-        echo "Installing MySQL is ... FAILURE"
-        exit 1
-    fi
+    VALIDATE $? "MySQL"
 else
     echo "MySQL is already installed...Nothing to do"
 fi
 
-dnf list installed pyhton3
-if [$? -ne 0 ]
+dnf list installed python3
+if [ $? -ne 0 ]
 then
-   echo "python3 is not installed .. going to install"
-   dnf install python3 -y
-   VALIDATE $? "python3"
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
 else
-   echo "python3 is already installed ... Nothing to do .."
+    echo "python3 is already installed...Nothing to do"
 fi
 
 dnf list installed nginx
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
-   echo "nginx is not installed ... going to install it"
-   dnf install nginx -y
-   VALIDATE $? "nginx"
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
 else
-   echo "nginx is already installed ... Nothing to do"
+    echo "nginx is already installed...Nothing to do"
 fi
