11c9d20f12c8ee08d5f5581dde04b02e2d17cc02
satyam7513 <satyamknaidu@gmail.com>
2026-03-09 17:30:11 +0530

    for loop

--- 15.for-loop.sh
+++ 15.for-loop.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 
-                 for i in {1..100}
-                 do
-                 echo $i
-                 done
+  for i in {1..100}
+    do
+    echo $i
+    done               
