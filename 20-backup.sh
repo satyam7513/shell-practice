dd84d27bd916e6c1f1888f9fd53ae32f9a4c0972
satyam7513 <satyamknaidu@gmail.com>
2026-03-31 22:39:53 +0530

    backup.sh

---
+++ 20-backup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+USERID=$(id -u)
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/backup.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+check_root(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR:: Please run this script with root access $N"
+        exit 1 #give other than 0 upto 127
+    else
+        echo "You are running with root access" 
+    fi
+}
+
+check_root
+
+mkdir -p $LOGS_FOLDER
+
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <destination-dir> <days>"
+    exit 1
+}
+
+if [ $# -lt 2 ]
+then
+   USAGE
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+  echo -e "$R Source Direcotry $SOURCE_DIR does not exist. please check $N"
+  exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then
+  echo -e "$R Destintion  Direcotry $DEST_DIR does not exist. please check $N"
+  exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+
+
